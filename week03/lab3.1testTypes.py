17be1afc939f4c8709b1fc842cb668fed09dfae0
kevinsenan <kevinsenan@hotmail.com>
2023-02-18 10:55:20 +0000

    update the files

---
+++ week03/lab3.1testTypes.py
@@ -0,0 +1,8 @@
+i = 3
+fl = 3.5
+isa = True
+memo = 'how now brown cow'
+lots = []
+
+print('variable {} is of type: {} and value {}'.format('i', type(i), i))
+print('variable {} is of type: {} and value: {}'.format('fl', type(fl), fl))
